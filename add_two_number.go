17ecaab2e41c395114764b3f42661a8286280a0a
kanin tansirisittikul <tan.kanin98@gmail.com>
2021-12-16 00:54:41 +0700

    init two_sum

---
+++ add_two_number.go
@@ -0,0 +1,16 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var res ListNode
+
+	for l1 != nil || l2 != nil {
+
+	}
+
+	return &res
+}
